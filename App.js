a50b8bf9f779b288d7f8320ee6472ae9a9fd1bce
Fiifi Yawson <ghyawson@gmail.com>
2023-10-14 18:49:43 +0000

    InservizHome

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import BottomNavigation from './navigation/BottomNavigation';
+import Notifications from './screens/Notifications';
+import MoreScreen from './screens/MoreScreen';
+import UserScreen from './screens/UserScreen';
+import EditScreen from './screens/EditScreen';
+import HelpScreen from './screens/HelpScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="BottomNavigation" component={BottomNavigation} options={{headerShown:false, tabBar:'visible'}}/>
+        <Stack.Screen name="Notifications" component={Notifications} options={{headerShown:false}}/>
+        <Stack.Screen name="More" component={MoreScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Profile" component={UserScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Edit" component={EditScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Help" component={HelpScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Settings" component={SettingsScreen} options={{headerShown:false}}/>
+   
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
